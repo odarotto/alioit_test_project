e814b63af0b6d82e9b778ad6fc59ea9ddcb4f273
odarotto <odarotto@gmail.com>
2022-07-26 17:00:22 -0400

    Creating basic structure for API

---
+++ pokeapi/aiohttp_pokeapi/settings.py
@@ -0,0 +1,13 @@
+from decimal import getcontext
+import pathlib
+import yaml
+
+BASE_DIR = pathlib.Path(__file__).parent.parent
+config_path = BASE_DIR / 'config' / 'pokeapi.yaml'
+
+def get_config(path):
+    with open(path) as f:
+        config = yaml.safe_load(f)
+    return config
+
+config = get_config(config_path)
